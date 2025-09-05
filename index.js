21c24f12b9c4253001ae6afe593ea00f7db1ff6c
Lewis Whitehill <lwhitehi@ucsc.edu>
2025-09-05 10:07:33 -0700

    post upload endpoint added, using multer for middleware

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const multer = require("multer");
+
 const app = express();
+// set the destination for photo uploads
+const upload = multer({ dest: 'uploads/'})
 
 const port = 8000;
-
 app.listen(port, () => {
     console.log(`Server is now listenign on ${port}`);
 });
 
 // get endpoint
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.send("Welcome to the photo uploader. Wow!");
 
+});
+
+// post endpoint, lets someone upload 20 photos max
+app.post('/upload', upload.array('photos', 20), (req, res, next) => {
+    try{
+        res.sendStatus(200);
+    }catch(err){
+        console.error(err)
+        res.sendStatus(500);
+    }
+   
 });

fc201ee04d9c6794748f6051386ea70852e17020
Lewis Whitehill <lwhitehi@ucsc.edu>
2025-09-16 14:52:11 -0700

    Changed file too big error message.

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.use((err, req, res, next) => {
     // if it's a multer error
     if(err instanceof multer.MulterError){
         if(err.code == "LIMIT_FILE_SIZE"){
-            return res.status(413).json({error : "File too large."})
+            return res.status(413).json({error : "File too large. Max size is 5MB"})
         }
         else{
             return res.status(401).json({error: "multer error"})

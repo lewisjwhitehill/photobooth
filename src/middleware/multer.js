7df6073e80e3e07fc15e941c1e36670a1663ffca
Lewis Whitehill <lwhitehi@ucsc.edu>
2025-09-30 14:38:29 -0700

    improved file structure.

---
+++ src/middleware/multer.js
@@ -0,0 +1,28 @@
+// set up multer storage configuration
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./images");
+    },
+    filename: (req, file, cb) => {
+        // taken from npm multer docs
+        const uniquePreffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        // preserve the orgininal file name but add a unique suffix
+        cb(null, uniquePreffix + '-' + file.originalname);
+    },
+});
+
+// set up multer file filtering configureation
+const fileFilterMiddleware = (req, file, cb) => {
+    // make sure the file type is an image file that they're uploading
+    // could change to file.mimetype.startsWith("image/")
+    if ((file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg" || file.mimetype === "application/octet-stream")) {
+        cb(null, true)
+    }  else {
+        cb(new Error('BAD_FILE_TYPE'), false)
+    }
+}
+
+// Give multer the storage configuration, individual file size limit and file type filtering function
+const upload = multer({ storage : fileStorageEngine, 
+                        limits: { fileSize: 5 * 1024 * 1024 },
+                        fileFilter: fileFilterMiddleware});
---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import multer from 'multer'
+import fs from 'fs'
+
+const app = express();
+
+const port = 8000 || process.env.port
+
+app.listen(port, () => {
+    console.log(`Server is now listening on ${port}`);
+});
+
+// get endpoint
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to the photo uploader. Wow!</h1>");
+});
+
+// Routes
+app.use('/photo', authRoutes)
+app.use('/todos', authMiddleware, todoRoutes)
+
+
+// Global error handler
+app.use((err, req, res, next) => {
+    // if it's a multer error
+    if(err instanceof multer.MulterError){
+        if(err.code == "LIMIT_FILE_SIZE"){
+            return res.status(413).json({error : "File too large. Max size is 5MB"})
+        }
+        else{
+            return res.status(401).json({error: "multer error"})
+        }
+    }
+    // if fileFilterer threw a bad file type
+    if(err.message == "BAD_FILE_TYPE"){
+        return res.status(400).json({error: "Unsupported file type. Image files only."})
+    }
+    // default server error
+    res.status(500).send("Something went wrong!");
+});
